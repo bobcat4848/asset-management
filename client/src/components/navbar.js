a54f88efe3af91e64cec655b712f11aa2aa253ec
Bobcat <bobcat4848@gmail.com>
2021-09-23 11:31:27 -0500

    Update navbar.js

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,17 +2,21 @@ import React from "react";
  
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
- 
+import "bootstrap/dist/js/bootstrap"; 
+
 // We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
+
+// Import logo from src/images
+import logo from "../images/logo.png"
  
 // Here, we display our Navbar
 const Navbar = () => {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div style={{paddingBottom: 25}}>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <NavLink className="navbar-brand" to="/">
-          MongoDB
+          <img src={logo} alt="" width="225" height="50"/>
         </NavLink>
         <button
           className="navbar-toggler"
@@ -29,9 +33,25 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <NavLink className="nav-link" to="/create">
-                Create Record
+              <NavLink className="nav-link" to="/">
+                Home
               </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">
+                Equipment
+              </NavLink>
+            </li>
+            <li className="nav-item dropdown">
+              <NavLink className="nav-link dropdown-toggle" to="/" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Manage
+              </NavLink>
+              <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                <li><NavLink className="dropdown-item" to="/users">Users</NavLink></li>
+                <li><NavLink className="dropdown-item" to="/items">Items</NavLink></li>
+                <li><hr className="dropdown-divider"/></li>
+                <li><NavLink className="dropdown-item" to="/system">System</NavLink></li>
+              </ul>
             </li>
           </ul>
         </div>
