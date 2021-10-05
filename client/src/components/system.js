cc0f4fdcca385792469a985e315e0f0e9349b9da
Karmen Freeman <karmenafreeman@gmail.com>
2021-10-05 10:38:21 -0500

    Created and linked system page

---
+++ client/src/components/system.js
@@ -0,0 +1,15 @@
+import React,{ Component } from "react";
+
+class System extends Component{
+  render(){
+    return(
+      <div>
+          <h1>Testing</h1>
+          <p>This is the admin page.</p>
+      </div>
+    );
+  }
+}
+  
+export default System;
+  
